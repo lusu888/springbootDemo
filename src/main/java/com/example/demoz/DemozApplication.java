0299aa4ea978c4e3ee84967ab7e5a112b2d9cd9d
Zhang Zhang <z.zhang@videotrend.local>
2024-05-31 14:07:40 +0200

    layout

---
+++ src/main/java/com/example/demoz/DemozApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemozApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demoz/controller/helloController.java
@@ -0,0 +1,21 @@
+package com.example.demoz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping
+public class helloController {
+
+    @GetMapping("/helloz")
+    public ModelAndView hello2(){
+        ModelAndView hlloaModelAndView = new ModelAndView("helloz");
+        hlloaModelAndView.addObject("zvalue","hello ZZ");
+        return hlloaModelAndView;
+    }
+
+
+}
